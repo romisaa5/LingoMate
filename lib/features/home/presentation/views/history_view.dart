e3e3ce3cfbfb9f95b14c57c5b00854274256a148
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-05-30 19:26:00 +0200

    done history view UI

--- lib/features/home/presentation/views/history_view.dart
+++ lib/features/home/presentation/views/history_view.dart
@@ -1,10 +1,44 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_transalate/core/theme/text_theme.dart';
+import 'package:google_transalate/features/home/presentation/widgets/history_item.dart';
 
 class HistoryView extends StatelessWidget {
   const HistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              'History View',
+              style: Styles.textStyle20.copyWith(color: Colors.black),
+            ),
+          ),
+          SizedBox(height: 10),
+          ListTile(
+            leading: Icon(Icons.history, color: Colors.black),
+            title: Text(
+              'Your Translation History',
+              style: Styles.textStyle12.copyWith(color: Colors.black54),
+            ),
+            trailing: TextButton(onPressed: (){}, child: Text('Clear All', style: Styles.textStyle12.copyWith(color: Colors.red,
+              fontWeight: FontWeight.bold)) 
+          ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10, // Example item count
+              itemBuilder: (context, index) {
+                return HistoryItem();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
